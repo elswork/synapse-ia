b92400e5a89795182c2169bc09469d256071f5cc
Eloy López <elswork@gmail.com>
2026-05-16 12:12:02 +0200

    feat: add cache-control headers to m2_status_api global response middleware

--- m2_status_api.py
+++ m2_status_api.py
@@ -601,6 +601,13 @@ def serve_static(filename):
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
     return response
 
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 if __name__ == '__main__':
     # Correr en puerto 5051 para no interferir con el trigger de Athena (5050)
     app.run(host='0.0.0.0', port=5051)
